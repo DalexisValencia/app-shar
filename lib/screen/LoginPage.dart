72fe31db3afac2a344ce2776ba9609c54db6b2d8
Duvan Alexis Valencia Munca <d.alexis.valencia@gmail.com>
2025-03-28 15:39:09 -0500

    login element

--- lib/screen/LoginPage.dart
+++ lib/screen/LoginPage.dart
@@ -35,35 +35,20 @@ class _LoginPageState extends State<LoginPage> with TickerProviderStateMixin {
     var screenWidth = mediaQuery.size.width;
 
     return Scaffold(
+      appBar: AppBar(),
       body: Wrapperblocintances(
-        childComponent: SingleChildScrollView(
-            child: Container(
-          padding: const EdgeInsets.symmetric(
-            vertical: 10,
-            horizontal: 20,
-          ),
-          child: Column(
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                height: screenWidth * 0.73,
-                child: Stack(
-                  children: [
-                    Positioned(
-                      top: 10,
-                      right: 20,
-                      child: IconButton(
-                        color: blackColor,
-                        icon: const Icon(Icons.close),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
+          childComponent: Container(
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 20,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            
+            Center(
+              child: Container(
                 width: 150,
                 margin: const EdgeInsets.symmetric(
                   vertical: 20,
@@ -73,128 +58,133 @@ class _LoginPageState extends State<LoginPage> with TickerProviderStateMixin {
                   fit: BoxFit.fitWidth,
                 ),
               ),
-              const Text(
+            ),
+            Container(
+              margin: const EdgeInsets.only(
+                top: 30,
+              ),
+              child: const Text(
                 'INGRESAR',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.start,
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
               ),
-              const SizedBox(height: 20),
-              TextField(
-                autofocus: false,
-                style: const TextStyle(
-                  fontSize: 12,
-                  color: blackColor,
-                ),
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: const Color.fromARGB(255, 207, 207, 207),
-                  hintText: 'Correo electrónico',
-                  contentPadding: const EdgeInsets.symmetric(
-                    vertical: 20,
-                    horizontal: 20,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      color: greyColor,
-                    ),
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: const BorderSide(
-                      color: greyLightColor,
-                    ),
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                ),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              autofocus: false,
+              style: const TextStyle(
+                fontSize: 12,
+                color: blackColor,
               ),
-              const SizedBox(height: 10),
-              TextField(
-                autofocus: false,
-                style: const TextStyle(
-                  fontSize: 12,
-                  color: blackColor,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: const Color.fromARGB(255, 207, 207, 207),
+                hintText: 'Correo electrónico',
+                contentPadding: const EdgeInsets.symmetric(
+                  vertical: 20,
+                  horizontal: 20,
                 ),
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: const Color.fromARGB(255, 207, 207, 207),
-                  hintText: 'Password',
-                  contentPadding: const EdgeInsets.symmetric(
-                    vertical: 20,
-                    horizontal: 20,
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: greyColor,
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      color: greyColor,
-                    ),
-                    borderRadius: BorderRadius.circular(5),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: greyLightColor,
                   ),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: const BorderSide(
-                      color: greyLightColor,
-                    ),
-                    borderRadius: BorderRadius.circular(5),
-                  ),
+                  borderRadius: BorderRadius.circular(5),
                 ),
               ),
-              Container(
-                margin: const EdgeInsets.only(
-                  top: 20,
-                  bottom: 5,
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              autofocus: false,
+              style: const TextStyle(
+                fontSize: 12,
+                color: blackColor,
+              ),
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: const Color.fromARGB(255, 207, 207, 207),
+                hintText: 'Password',
+                contentPadding: const EdgeInsets.symmetric(
+                  vertical: 20,
+                  horizontal: 20,
                 ),
-                width: double.infinity,
-                height: 50,
-                child: TextButton(
-                  style: ButtonStyle(
-                    shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                      const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(5),
-                        ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: greyColor,
+                  ),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: greyLightColor,
+                  ),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(
+                top: 20,
+                bottom: 5,
+              ),
+              width: double.infinity,
+              height: 50,
+              child: TextButton(
+                style: ButtonStyle(
+                  shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                    const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5),
                       ),
                     ),
-                    backgroundColor: WidgetStateProperty.all<Color>(blackColor),
                   ),
-                  onPressed: () {},
-                  child: const Text(
-                    'INGRESAR',
-                    style: TextStyle(
-                      color: whiteColor,
-                      fontWeight: FontWeight.w600,
-                    ),
+                  backgroundColor: WidgetStateProperty.all<Color>(blackColor),
+                ),
+                onPressed: () {},
+                child: const Text(
+                  'INGRESAR',
+                  style: TextStyle(
+                    color: whiteColor,
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
               ),
-              Container(
-                margin: const EdgeInsets.only(
-                  bottom: 20,
-                ),
-                width: double.infinity,
-                height: 50,
-                child: TextButton(
-                  style: ButtonStyle(
-                    shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                      const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(5),
-                        ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(
+                bottom: 20,
+              ),
+              width: double.infinity,
+              height: 50,
+              child: TextButton(
+                style: ButtonStyle(
+                  shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                    const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5),
                       ),
                     ),
-                    backgroundColor:
-                        WidgetStateProperty.all<Color>(yellowColor),
                   ),
-                  onPressed: () {},
-                  child: const Text(
-                    'REGISTRARSE',
-                    style: TextStyle(
-                      color: blackColor,
-                      fontWeight: FontWeight.w600,
-                    ),
+                  backgroundColor: WidgetStateProperty.all<Color>(yellowColor),
+                ),
+                onPressed: () {},
+                child: const Text(
+                  'REGISTRARSE',
+                  style: TextStyle(
+                    color: blackColor,
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
-              )
-            ],
-          ),
-        )),
-      ),
+              ),
+            ),
+          ],
+        ),
+      )),
     );
   }
 }
