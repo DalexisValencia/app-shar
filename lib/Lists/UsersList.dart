2f7a75497c38318aded062831b7ede270310f2cc
Duvan Alexis Valencia Munca <d.alexis.valencia@gmail.com>
2025-03-29 22:10:52 -0500

    Add users test

---
+++ lib/Lists/UsersList.dart
@@ -0,0 +1,16 @@
+import 'package:shar/interfaces/User.dart';
+
+List<UserInterface> UserList = [
+  UserInterface(
+    email: "test@gmail.com",
+    password: "123"
+  ),
+  UserInterface(
+    email: "test-1@gmail.com",
+    password: "234"
+  ),
+  UserInterface(
+    email: "test-2@gmail.com",
+    password: "456"
+  ),
+];
