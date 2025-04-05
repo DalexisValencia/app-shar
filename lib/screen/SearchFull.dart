b5713d3c4a759b7f21381f9a38393c96c95dffce
Duvan Alexis Valencia Munca <d.alexis.valencia@gmail.com>
2025-04-05 15:48:35 -0500

    Fix safe area

--- lib/screen/SearchFull.dart
+++ lib/screen/SearchFull.dart
@@ -28,93 +28,95 @@ class _SearchfullState extends State<Searchfull> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            TextFormField(
-              controller: searchingText,
-              enableSuggestions: false,
-              autocorrect: false,
-              autofocus: false,
-              style: const TextStyle(
-                fontSize: 12,
-                color: blackColor,
-              ),
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: const Color.fromARGB(255, 238, 238, 238),
-                hintText: '¿Qué estás buscando?',
-                contentPadding: const EdgeInsets.symmetric(
-                  vertical: 20,
-                  horizontal: 20,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextFormField(
+                controller: searchingText,
+                enableSuggestions: false,
+                autocorrect: false,
+                autofocus: false,
+                style: const TextStyle(
+                  fontSize: 12,
+                  color: blackColor,
                 ),
-                errorStyle: const TextStyle(
-                  fontSize: 9,
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(
-                    color: greyColor,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: const Color.fromARGB(255, 238, 238, 238),
+                  hintText: '¿Qué estás buscando?',
+                  contentPadding: const EdgeInsets.symmetric(
+                    vertical: 20,
+                    horizontal: 20,
                   ),
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: const BorderSide(
-                    color: greyLightColor,
+                  errorStyle: const TextStyle(
+                    fontSize: 9,
                   ),
-                  borderRadius: BorderRadius.circular(5),
-                ),
-              ),
-              onChanged: (value) => {
-                print(value),
-                productsBlocIntance.add(
-                  ProductsByFilter(
-                    term: value,
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      color: greyColor,
+                    ),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: const BorderSide(
+                      color: greyLightColor,
+                    ),
+                    borderRadius: BorderRadius.circular(5),
                   ),
                 ),
-              },
-            ),
-            const Fadetransitionwrapper(
-              durationTime: 800,
-              widgetChild: Categorychipwrapper(),
-            ),
-            Container(
-              margin: const EdgeInsets.symmetric(
-                vertical: 10,
-                horizontal: 20,
-              ),
-              child: BlocBuilder<ProductsBloc, ProductsState>(
-                builder: (BuildContext context, ProductsState state) {
-                  List<ProductsInterface> resultsFilter =
-                      state.props[2] as List<ProductsInterface>;
-                  if (resultsFilter.isEmpty) {
-                    return const Fallbacks(
-                      description:
-                          "Lo sentimos, no hay resultados para la búsqueda que ha realizado.",
-                    );
-                  }
-
-                  return Builder(
-                    builder: (BuildContext context) {
-                      List<Widget> productsFinal = [];
-                      resultsFilter.asMap().entries.map((e) {
-                        productsFinal.add(
-                          ProductCard(
-                            isMiddlePage: true,
-                            product: e.value,
-                          ),
-                        );
-                      }).toList();
-                      productsFinal.add(
-                          const Fallbacks(description: "No hay más productos"));
-                      return Wrap(
-                        children: productsFinal,
-                      );
-                    },
-                  );
+                onChanged: (value) => {
+                  print(value),
+                  productsBlocIntance.add(
+                    ProductsByFilter(
+                      term: value,
+                    ),
+                  ),
                 },
               ),
-            )
-          ],
+              const Fadetransitionwrapper(
+                durationTime: 800,
+                widgetChild: Categorychipwrapper(),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  vertical: 5,
+                  horizontal: 20,
+                ),
+                child: BlocBuilder<ProductsBloc, ProductsState>(
+                  builder: (BuildContext context, ProductsState state) {
+                    List<ProductsInterface> resultsFilter =
+                        state.props[2] as List<ProductsInterface>;
+                    if (resultsFilter.isEmpty) {
+                      return const Fallbacks(
+                        description:
+                            "Lo sentimos, no hay resultados para la búsqueda que ha realizado.",
+                      );
+                    }
+
+                    return Builder(
+                      builder: (BuildContext context) {
+                        List<Widget> productsFinal = [];
+                        resultsFilter.asMap().entries.map((e) {
+                          productsFinal.add(
+                            ProductCard(
+                              isMiddlePage: true,
+                              product: e.value,
+                            ),
+                          );
+                        }).toList();
+                        productsFinal.add(const Fallbacks(
+                            description: "No hay más productos"));
+                        return Wrap(
+                          children: productsFinal,
+                        );
+                      },
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
