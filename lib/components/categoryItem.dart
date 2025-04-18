ffb6a88f51f94bf75ef31451930a0d8a23d3f685
Duvan Alexis Valencia Munca <d.alexis.valencia@gmail.com>
2025-04-18 13:02:15 -0500

    Link in categories Home

--- lib/components/categoryItem.dart
+++ lib/components/categoryItem.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shar/blocs/favorites/products_bloc.dart';
 import 'package:shar/interfaces/CategoryInterface.dart';
 import 'package:shar/constants/contants.dart';
+import 'package:shar/screen/SearchFull.dart';
 
-class CategoryItem extends StatelessWidget {
+class CategoryItem extends StatefulWidget {
   final CategoryInterface category;
   const CategoryItem({
     super.key,
     required this.category,
   });
+
+  @override
+  State<CategoryItem> createState() => _CategoryItemState();
+}
+
+class _CategoryItemState extends State<CategoryItem> {
+  late ProductsBloc productsBlocIntance;
+
+  @override
+  void initState() {
+    super.initState();
+    productsBlocIntance = BlocProvider.of<ProductsBloc>(context);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +37,18 @@ class CategoryItem extends StatelessWidget {
         width: screenWidth * 0.275,
         height: screenWidth * 0.213,
         child: InkWell(
-          onTap: () {},
+          onTap: () {
+            productsBlocIntance.add(
+              ProductsByCatergory(
+                category: widget.category,
+              ),
+            );
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => const Searchfull(),
+              ),
+            );
+          },
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
@@ -29,7 +56,7 @@ class CategoryItem extends StatelessWidget {
               SizedBox(
                 width: screenWidth * 0.10,
                 child: Image(
-                  image: AssetImage(category.image),
+                  image: AssetImage(widget.category.image),
                   fit: BoxFit.cover,
                 ),
               ),
@@ -37,7 +64,7 @@ class CategoryItem extends StatelessWidget {
                 margin: EdgeInsets.only(top: screenWidth * 0.02),
                 padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
                 child: Text(
-                  category.name,
+                  widget.category.name,
                   textScaler: const TextScaler.linear(0.67),
                   textAlign: TextAlign.center,
                 ),
